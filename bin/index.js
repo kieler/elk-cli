d0c1b01a85f53af510d54827bffd76a9a1cc299f
Max Kasperowski <mka@informatik.uni-kiel.de>
2024-01-19 08:26:30 +0100

    initial commit

---
+++ bin/index.js
@@ -0,0 +1,39 @@
+const fs = require("fs");
+const yargs = require("yargs");
+const ELK = require("elkjs");
+
+const elk = new ELK();
+
+const usage = "Supply an elkj graph directly with the -g option or read in an elkj graph from a file with the -f option."
+const options = yargs.usage(usage)
+                     .option("g", {alias: "graph", describe: "Input elkj graph", type: "string", demandOption: false})
+                     .option("f", {alias: "file", describe: "Input file", type: "string", demandOption: false})
+                     .help(true)
+                     .argv;
+
+let graph = {}
+if (options.g !== undefined) {
+    console.log(options.g);
+    graph = JSON.parse(options.g);
+
+} else if (options.f !== undefined) {
+    s = fs.readFileSync(options.f, "utf8");
+    // Preserve newlines, etc. - use valid JSON
+    s = s.replace(/\\n/g, "\\n")
+    .replace(/\\'/g, "\\'")
+    .replace(/\\"/g, '\\"')
+    .replace(/\\&/g, "\\&")
+    .replace(/\\r/g, "\\r")
+    .replace(/\\t/g, "\\t")
+    .replace(/\\b/g, "\\b")
+    .replace(/\\f/g, "\\f");
+    // Remove non-printable and other non-valid JSON characters
+    s = s.replace(/[\u0000-\u0019]+/g,"");
+    graph = JSON.parse(s);
+} else {
+    console.log("Please supply an elkg graph. Use --help for usage information.")
+    process.exit();
+}
+elk.layout(graph)
+       .then(console.log)
+       .catch(console.error);
